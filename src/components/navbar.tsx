9f87a5a89543b4112cbc08be8dff079ad35e56d1
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-09-04 22:20:28 +0700

    fix: navbar positioningn

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,20 @@
 const Navbar = () => {
   return (
-    <nav className="flex justify-between p-4 place-items-center">
-      <h3 className="font-bold text-3xl cursor-pointer">
+    <nav className="py-8 mx-10 relative">
+      <h3 className="font-bold text-3xl cursor-pointer mb-4 absolute left-0 top-1/2 -translate-y-1/2">
         <a href="/">CollabIn</a>
       </h3>
-      <ul className="flex justify-between xl:w-1/3 2xl:w-1/4 cursor-pointer navbar">
-        <li>
+      <ul className="flex gap-4 items-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 navbar">
+        <li className="py-1 px-[.7rem]">
           <a href="/">HOMEPAGE</a>
         </li>
-        <li>
+        <li className="py-1 px-[.7rem]">
           <a href="/projects">PROJECTS</a>
         </li>
-        <li>
+        <li className="py-1 px-[.7rem]">
           <a href="/my-projects">MY PROJECTS</a>
         </li>
-        <li>
+        <li className="py-1 px-[.7rem]">
           <a href="/bookmarks">BOOKMARKS</a>
         </li>
       </ul>
