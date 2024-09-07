6091f615b4355ac2bdb439d38d07fb95fbd730ac
Arif Amijoyo <arifamjy@gmail.com>
2024-09-07 14:10:14 +0700

    feat: project detail page

---
+++ src/pages/projects/[id].tsx
@@ -0,0 +1,89 @@
+import Button from '../../components/button'
+
+const ProjectDetail = () => {
+  return (
+    <div className="mb-96">
+      <div className="w-full h-[650px] px-24 relative">
+        <img
+          src="/project-image-3.png"
+          className="w-full h-full object-cover"
+          alt="project cover"
+        />
+        <div className="absolute inset-0 bg-text w-full h-[300px] -z-10" />
+      </div>
+      <section className="mt-24 px-32 container mx-auto">
+        <div className="flex justify-between">
+          <p>JOHN DOE -- PROJECT TITLE</p>
+          <p>THE CATEGORY</p>
+        </div>
+        <div className="px-32">
+          <p className="my-12">
+            17th anniversary remaster. New sleeve artwork with photos by Steve
+            Gullick and pressed on dirty white vinyl. Ladybird … one perfect
+            heavy as fuck hypnotic riff played over and over and over and over
+            across 2 sides of an LP. Back when Shit And Shine formed, the idea
+            was basically to sound like their heroes DRUNKS WITH GUNS and
+            STRANGULATED BEATOFFS, using that same basic formula. A big dumb ass
+            catchy riff played over and over until the joke gets old … then keep
+            going. Recorded live at Southern Studios, London (fun fact: the same
+            room where BAUHAUS recorded ‘Bela Lugosi’s Dead’!) in 2004 with just
+            two basses, a snare drum, a huge cardboard box and a tiny toy Casio
+            sa-1 keyboard (using the “airplane” sound hitting the same key over
+            and over for 41 minutes) Some still say it’s the best thing shit and
+            shine has ever done. I’m not arguing. Originally released on CD, and
+            super limited vinyl waaay back in 2005. This was Shit And Shine’s
+            second release and it sold out REAL quick. Remastered and beefed the
+            hell up by Craig Clouse at Shit And Shine Ranch 2022.
+          </p>
+          <div className="flex gap-6">
+            <img
+              src="/project-image-5.png"
+              width="150"
+              height="150"
+              className="object-cover"
+              alt="other project attachment media"
+            />
+            <img
+              src="/project-image-6.png"
+              width="150"
+              height="150"
+              className="object-cover"
+              alt="other project attachment media"
+            />
+          </div>
+        </div>
+
+        <h2 className="text-7xl mt-24 text-secondary font-extrabold">
+          HOW DO YOU CONTRIBUTE TO THIS PROJECT?
+        </h2>
+        <p className="px-32 my-12">
+          17th anniversary remaster. New sleeve artwork with photos by Steve
+          Gullick and pressed on dirty white vinyl. Ladybird … one perfect heavy
+          as fuck hypnotic riff played over and over and over and over across 2
+          sides of an LP. Back when Shit And Shine formed, the idea was
+          basically to sound like their heroes DRUNKS WITH GUNS and STRANGULATED
+          BEATOFFS, using that same basic formula. A big dumb ass catchy riff
+          played over and over until the joke gets old … then keep going.
+          Recorded live at Southern Studios, London (fun fact: the same room
+          where BAUHAUS recorded ‘Bela Lugosi’s Dead’!) in 2004 with just two
+          basses, a snare drum, a huge cardboard box and a tiny toy Casio sa-1
+          keyboard (using the “airplane” sound hitting the same key over and
+          over for 41 minutes) Some still say it’s the best thing shit and shine
+          has ever done. I’m not arguing. Originally released on CD, and super
+          limited vinyl waaay back in 2005. This was Shit And Shine’s second
+          release and it sold out REAL quick. Remastered and beefed the hell up
+          by Craig Clouse at Shit And Shine Ranch 2022.
+        </p>
+        <div className="mt-32 flex justify-between">
+          <div className="flex items-center">
+            <img src="/instagram-logo.svg" alt="instagram logo" />
+            <span className="text-xl">@arifamjy</span>
+          </div>
+          <Button>CONTRIBUTE NOW</Button>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default ProjectDetail
