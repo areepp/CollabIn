318130d6f39a935e3344ae48496472db8870592f
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-09-09 11:44:35 +0700

    feat: responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,37 +1,271 @@
-const Navbar = () => {
+import clsx from 'clsx'
+import { useEffect, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+
+const NAV_LINKS = [
+  {
+    title: 'HOMEPAGE',
+    href: '/',
+  },
+  {
+    title: 'PROJECTS',
+    href: '/projects',
+  },
+  {
+    title: 'MY PROJECTS',
+    href: '/my-projects',
+  },
+  {
+    title: 'BOOKMARKS',
+    href: '/bookmarks',
+  },
+]
+
+const MENU_CONTAINER_VARIANTS = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+}
+
+const LINK_VARIANTS = {
+  initial: {
+    y: '10vh',
+  },
+  animate: {
+    y: 0,
+    transition: {
+      ease: [0, 0.55, 0.45, 1],
+    },
+  },
+}
+
+const LINK_WRAPPER_VARIANTS = {
+  initial: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDirection: -1,
+    },
+  },
+  animate: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDirection: 1,
+    },
+  },
+}
+
+type MenuProps = {
+  isMenuOpen: boolean
+}
+
+type MenuButtonProps = {
+  isMenuOpen: boolean
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const MenuButton = ({ isMenuOpen, setIsMenuOpen }: MenuButtonProps) => {
+  const variant = isMenuOpen ? 'opened' : 'closed'
+
+  const WIDTH = 24
+  const HEIGHT = 24
+  const UNIT_HEIGHT = 5
+  const UNIT_WIDTH = 4
+
+  const TOP_VARIANTS = {
+    closed: {
+      rotate: 0,
+      translateY: 0,
+      stroke: '#e4e4e7',
+    },
+    opened: {
+      rotate: 45,
+      translateY: 2,
+      stroke: '#129092',
+    },
+  }
+  const CENTER_VARIANTS = {
+    closed: {
+      opacity: 1,
+      stroke: '#e4e4e7',
+    },
+    opened: {
+      opacity: 0,
+      stroke: '#129092',
+    },
+  }
+  const BOTTOM_VARIANTS = {
+    closed: {
+      rotate: 0,
+      translateY: 0,
+      stroke: '#e4e4e7',
+    },
+    opened: {
+      rotate: -45,
+      translateY: -2,
+      stroke: '#129092',
+    },
+  }
+
+  const LINE_PROPS = {
+    strokeWidth: 2.5,
+    vectorEffect: 'non-scaling-stroke',
+    initial: 'closed',
+    animate: variant,
+    transition: {
+      duration: 0.3,
+    },
+  }
+
   return (
-    <nav className="py-8 relative" id="navbar">
-      <h3 className="left-10 font-bold text-3xl cursor-pointer mb-4 absolute top-1/2 -translate-y-1/2">
-        <a href="/">CollabIn</a>
-      </h3>
-      <ul className="flex gap-4 items-center cursor-pointer absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 navbar">
-        <a
-          href="/"
-          className="py-1 px-4 hover:bg-text hover:text-background rounded-sm"
+    <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+      <motion.svg
+        viewBox={`0 0 ${UNIT_WIDTH} ${UNIT_HEIGHT}`}
+        overflow="visible"
+        preserveAspectRatio="none"
+        width={WIDTH}
+        height={HEIGHT}
+        aria-label="toggle menu"
+      >
+        <motion.line
+          x1="0"
+          x2={UNIT_WIDTH}
+          y1="0"
+          y2="0"
+          variants={TOP_VARIANTS}
+          {...LINE_PROPS}
+        />
+        <motion.line
+          x1="0"
+          x2={UNIT_WIDTH}
+          y1="2"
+          y2="2"
+          variants={CENTER_VARIANTS}
+          {...LINE_PROPS}
+        />
+        <motion.line
+          x1="0"
+          x2={UNIT_WIDTH}
+          y1="4"
+          y2="4"
+          variants={BOTTOM_VARIANTS}
+          {...LINE_PROPS}
+        />
+      </motion.svg>
+    </button>
+  )
+}
+
+const Menu = ({ isMenuOpen }: MenuProps) => {
+  return (
+    <AnimatePresence>
+      {isMenuOpen && (
+        <motion.div
+          variants={MENU_CONTAINER_VARIANTS}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          className="fixed left-0 top-0 h-screen w-screen bg-accent pt-[150px] z-10 bg-background"
         >
-          HOMEPAGE
-        </a>
-        <a
-          href="/projects"
-          className="py-1 px-4 hover:bg-text hover:text-background rounded-sm"
+          <motion.nav
+            variants={LINK_WRAPPER_VARIANTS}
+            initial="initial"
+            animate="animate"
+            className="flex h-full flex-col gap-12 px-12 text-text sm:px-24 lg:px-48"
+          >
+            {NAV_LINKS.map(({ href, title }) => (
+              <div key={title} className="overflow-hidden">
+                <motion.h2
+                  variants={LINK_VARIANTS}
+                  className="w-fit text-2xl font-bold transition duration-300 hover:text-secondary sm:text-4xl lg:text-6xl"
+                >
+                  <a href={href}>{title}</a>
+                </motion.h2>
+              </div>
+            ))}
+          </motion.nav>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  )
+}
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth)
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <>
+      {width < 1024 ? (
+        <>
+          <header className="fixed left-0 top-0 z-50 flex h-[80px] w-full items-center bg-background">
+            <div className="mb-14 flex h-[30px] w-full items-center justify-between px-4 sm:mb-0 sm:px-8 lg:px-16">
+              <a
+                href="/"
+                className={clsx(
+                  'text-3xl font-bold transition duration-500 lg:text-2xl',
+                  isMenuOpen ? 'text-secondary' : 'text-text  '
+                )}
+              >
+                CollabIn
+              </a>
+              <MenuButton
+                isMenuOpen={isMenuOpen}
+                setIsMenuOpen={setIsMenuOpen}
+              />
+            </div>
+          </header>
+          <Menu isMenuOpen={isMenuOpen} />
+        </>
+      ) : (
+        <nav
+          className="py-8 h-[80px] fixed top-0 inset-x-0 z-50 bg-background"
+          id="navbar"
         >
-          PROJECTS
-        </a>
-        <a
-          href="/my-projects"
-          className="py-1 px-4 hover:bg-text hover:text-background rounded-sm"
-        >
-          MY PROJECTS
-        </a>
-        <a
-          href="/bookmarks"
-          className="py-1 px-4 hover:bg-text hover:text-background rounded-sm"
-        >
-          BOOKMARKS
-        </a>
-      </ul>
-      <div></div>
-    </nav>
+          <h3 className="left-10 font-bold text-3xl cursor-pointer mb-4 absolute top-1/2 -translate-y-1/2">
+            <a href="/">CollabIn</a>
+          </h3>
+          <ul className="flex gap-4 items-center cursor-pointer absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 navbar">
+            <a
+              href="/"
+              className="py-1 px-4 hover:bg-text hover:text-background rounded-sm"
+            >
+              HOMEPAGE
+            </a>
+            <a
+              href="/projects"
+              className="py-1 px-4 hover:bg-text hover:text-background rounded-sm"
+            >
+              PROJECTS
+            </a>
+            <a
+              href="/my-projects"
+              className="py-1 px-4 hover:bg-text hover:text-background rounded-sm"
+            >
+              MY PROJECTS
+            </a>
+            <a
+              href="/bookmarks"
+              className="py-1 px-4 hover:bg-text hover:text-background rounded-sm"
+            >
+              BOOKMARKS
+            </a>
+          </ul>
+          <div></div>
+        </nav>
+      )}
+    </>
   )
 }
 
