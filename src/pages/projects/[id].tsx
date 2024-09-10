6a5f34c1eb4dbfa963620e66e7c86d2516fa2d2b
Arif Amijoyo <arifamjy@gmail.com>
2024-09-10 17:43:03 +0700

    feat: responsive project detail

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -14,10 +14,10 @@ const ProjectDetail = ({
 
   return (
     <div className="mb-[550px]">
-      <div className="w-full h-[650px] relative">
+      <div className="w-full max-h-[650px] min-h-[230px] h-[50vw] relative">
         <img
           src={PROJECT_DATA.projects[id].media[0]}
-          className="w-full h-full object-cover px-24"
+          className="w-full h-full object-cover px-6 sm:px-12 lg:px-24"
           alt="project cover"
         />
         <div className="absolute inset-0 bg-text w-full h-[200px] -z-10" />
@@ -27,24 +27,26 @@ const ProjectDetail = ({
           className="absolute w-full top-28 -z-10"
         />
       </div>
-      <section className="relative mt-24 px-32 container mx-auto">
-        <div className="flex justify-between text-secondary">
+      <section className="relative mt-12 sm:mt-24 px-4 sm:px-8 lg:px-32 container mx-auto">
+        <div className="flex flex-col gap-3 sm:gap-0 sm:flex-row sm:justify-between text-secondary">
           <p>
             {PROJECT_DATA.projects[id].author} --{' '}
             {PROJECT_DATA.projects[id].title}
           </p>
-          <div className="flex gap-10">
+          <div className="flex flex-col gap-3 sm:flex-row sm:gap-10">
             <p>{PROJECT_DATA.projects[id].category}</p>
-            <img
-              src="/bookmark-icon.svg"
-              alt="bookmark icon"
-              className="cursor-pointer"
-            />
+            <div className="w-fit">
+              <img
+                src="/bookmark-icon.svg"
+                alt="bookmark icon"
+                className="cursor-pointer"
+              />
+            </div>
           </div>
         </div>
-        <div className="px-44">
+        <div className="sm:px-12 lg:px-44">
           <p className="my-12">{PROJECT_DATA.projects[id].description}</p>
-          <div className="flex gap-6">
+          <div className="flex gap-6 flex-wrap justify-center">
             {PROJECT_DATA.projects[id].media.map((media, index) => (
               <img
                 key={index}
@@ -58,13 +60,13 @@ const ProjectDetail = ({
           </div>
         </div>
 
-        <h2 className="text-7xl px-32 mt-24 text-secondary font-extrabold">
+        <h2 className="text-3xl sm:text-5xl lg:text-7xl sm:px-16 lg:px-32 mt-24 text-secondary font-extrabold">
           HOW DO YOU CONTRIBUTE TO THIS PROJECT?
         </h2>
-        <p className="px-44 my-12">
+        <p className="sm:px-20 lg:px-44 my-12">
           {PROJECT_DATA.projects[id].job_description}
         </p>
-        <div className="relative mt-32 flex justify-between">
+        <div className="relative mt-24 sm:mt-32 flex flex-col sm:flex-row sm:justify-between gap-12 sm:gap-0">
           <div className="flex items-center">
             <img src="/instagram-logo.svg" alt="instagram logo" />
             <span className="text-xl">
@@ -79,7 +81,7 @@ const ProjectDetail = ({
                 FINISH PROJECT
               </Button>
               {showContributeForm && (
-                <div className="absolute top-20 right-0 w-1/2 flex flex-col gap-6">
+                <div className="absolute top-full pt-6 right-0 w-full lg:w-1/2 flex flex-col gap-3 lg:gap-6">
                   <TextInput
                     label="CONTRIBUTORS"
                     placeholder="Enter the contributors of this project (separated by commas, e.g., John Doe, Quentin)"
@@ -96,7 +98,7 @@ const ProjectDetail = ({
                 CONTRIBUTE NOW
               </Button>
               {showContributeForm && (
-                <div className="absolute top-full right-0 w-1/2 flex flex-col gap-9">
+                <div className="absolute top-full pt-6 right-0 w-full lg:w-1/2 flex flex-col gap-3 lg:gap-9">
                   <TextInput label="NAME" placeholder="Enter your name" />
                   <TextInput
                     label="EMAIL"
